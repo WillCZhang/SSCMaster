c482ec399daf819e3e3c765972f4218c44b33b34
WillCZhang <will.chi.zhang@gmail.com>
2017-10-15 01:07:08 -0700

    Initial commit

---
+++ app/src/main/java/will/sscmaster/MainActivity.java
@@ -0,0 +1,87 @@
+package will.sscmaster;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import will.sscmaster.SupportingUI.MainListAdapter;
+
+public class MainActivity extends AppCompatActivity {
+    public static final String FACULTY_LIST = "faculty_list";
+
+    private String[] facultyData;
+    private RecyclerView mainList;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initData();
+        initView();
+    }
+
+    private void initData() {
+        facultyData = readFacultyList();
+    }
+
+    private void initView() {
+        mainList = (RecyclerView) findViewById(R.id.mainlist);
+        mLayoutManager = new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL);
+        mainList.setLayoutManager(mLayoutManager);
+        mAdapter = new MainListAdapter(facultyData, this);
+        mainList.setAdapter(mAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.navigation, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // TODO: 1. search course/instructor/buildings 2.tools(registration, calendar, etc.) 3. setting
+        switch (item.getItemId()) {
+            case R.id.search:
+                Toast.makeText(this, "clicked", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    // return an array of string in a form of "faculty_name,color"
+    private String[] readFacultyList() {
+        try {
+            StringBuilder stringBuilder = new StringBuilder();
+            InputStreamReader inputStreamReader = new InputStreamReader(getAssets().open(FACULTY_LIST), "UTF-8");
+            BufferedReader br = new BufferedReader(inputStreamReader);
+            String line = br.readLine();
+            while (line != null) {
+                stringBuilder.append(line);
+                line = br.readLine();
+            }
+            br.close();
+            inputStreamReader.close();
+            String temp = stringBuilder.toString();
+            return temp.split(";");
+        } catch(IOException e) {
+            return new String[1];
+        }
+    }
+}
---
+++ app/src/main/java/will/sscmaster/SupportingUI/MainListAdapter.java
@@ -0,0 +1,89 @@
+package will.sscmaster.SupportingUI;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Point;
+import android.support.v7.widget.RecyclerView;
+import android.view.Display;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Button;
+
+import java.util.List;
+import java.util.Random;
+
+import will.sscmaster.R;
+
+/**
+ * Created by Will on 2017/8/5.
+ */
+
+public class MainListAdapter extends RecyclerView.Adapter<MainListAdapter.ViewHolder> {
+    private String[] dataset;
+    private String[] colorset;
+    private Context context;
+    private Random mRandom = new Random();
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        Button button;
+        public ViewHolder(View itemView) {
+            super(itemView);
+            button = (Button) itemView.findViewById(R.id.faculty);
+        }
+    }
+
+    public MainListAdapter (String[] dataset, Context context) {
+        preHandleData(dataset);
+        this.context = context;
+    }
+
+    private void preHandleData(String[] dataset) {
+        this.dataset = new String[dataset.length];
+        this.colorset = new String[dataset.length];
+
+        for (int i = 0; i < dataset.length; i++) {
+            this.dataset[i] = dataset[i].split(",")[0];
+            this.colorset[i] = dataset[i].split(",")[1];
+        }
+    }
+
+    @Override
+    public MainListAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.main_list, parent, false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(MainListAdapter.ViewHolder holder, int position) {
+        setWidthHeight(holder);
+        setData(holder, position);
+    }
+
+    private void setWidthHeight(ViewHolder holder) {
+        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        Display display = wm.getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+
+        // TODO: find a nicer way to get a random size of height for the button
+//        holder.button.getLayoutParams().height = getRandomIntInRange(250,180);
+        holder.button.getLayoutParams().height = (size.y / 5) + mRandom.nextInt(size.y / 5);
+        holder.button.getLayoutParams().width = (size.x / 2 ) - size.x / 16;
+    }
+
+    private void setData(ViewHolder holder, int position) {
+        holder.button.setText(dataset[position]);
+        holder.button.setBackgroundColor(Color.parseColor(colorset[position]));
+    }
+
+    @Override
+    public int getItemCount() {
+        return dataset.length;
+    }
+
+    private int getRandomIntInRange(int max, int min){
+        return mRandom.nextInt((max-min)+min)+min;
+    }
+}
