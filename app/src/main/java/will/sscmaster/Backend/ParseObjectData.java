5f1d794b0fa623a6d477277606ff7de518b35ddc
WillCZhang <will.chi.zhang@gmail.com>
2017-10-17 14:07:23 -0700

    Can read data

---
+++ app/src/main/java/will/sscmaster/Backend/ParseObjectData.java
@@ -0,0 +1,82 @@
+package will.sscmaster.Backend;
+
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.sql.Time;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class ParseObjectData {
+    private static JSONArray jsonArray;
+
+    public static Set<CourseObject> parseJson(String jsonString) {
+        Set<CourseObject> result = new HashSet<>();
+        try {
+            jsonArray = new JSONArray(jsonString);
+            createCourseObject(result);
+        } catch (Exception e) {
+            Log.e("ERROR", "Got an error when parsing JSON");
+        }
+        return result;
+    }
+
+    private static void createCourseObject(Set<CourseObject> result) {
+        for (int i = 0; i < jsonArray.length(); i++) {
+            try {
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                CourseObject temp = new CourseObject(jsonObject.getString("departmentShortName"), jsonObject.getString("courseNumber"));
+                temp.setDepartmentFullName(jsonObject.getString("departmentFullName"));
+                temp.setFaculty(jsonObject.getString("faculty"));
+                temp.setCourseName(jsonObject.getString("courseName"));
+                temp.setCredits(jsonObject.getString("credits"));
+                temp.setDescription(jsonObject.getString("description"));
+                temp.setReqs(jsonObject.getString("reqs"));
+//                creatSectionObject(temp, jsonObject.getJSONArray("sections"));
+
+                result.add(temp);
+            } catch (JSONException e) {
+                Log.e("ERROR", "Got an error when storing data");
+            }
+        }
+    }
+
+    private static void creatSectionObject(CourseObject courseObject, JSONArray sections) throws JSONException {
+        Set<SectionObject> sectionlist = new HashSet<>();
+        for (int i = 0; i < sections.length(); i++) {
+            JSONObject sectionJson = sections.getJSONObject(i);
+            SectionObject sectionObject = new SectionObject(sectionJson.getString("section"), sectionJson.getString("status"),
+                    sectionJson.getString("activity"), sectionJson.getString("term"));
+
+            sectionObject.setInstructor(sectionJson.getString("instructor"));
+            sectionObject.setInstructorWebsite(sectionJson.getString("instructorWebsite"));
+            sectionObject.setClassroom(sectionJson.getString("classroom"));
+            sectionObject.setRestrictTo(sectionJson.getString("restrictTo"));
+            sectionObject.setLastWithdraw(sectionJson.getString("lastWithdraw"));
+            sectionObject.setSeatsInfo(sectionJson.getInt("totalSeats"), sectionJson.getInt("currentRegistered"),
+                    sectionJson.getInt("restrictSeats"), sectionJson.getInt("generalSeats"));
+
+            JSONArray timeMap = sectionJson.getJSONArray("timeMap");
+            if (timeMap != null) {
+                Map<String, String> temp = new HashMap<>();
+                for (int n = 0; n < timeMap.length(); n++) {
+                    JSONObject jsonObject = timeMap.getJSONObject(n);
+                    while (jsonObject.keys().hasNext()) {
+                        String key = jsonObject.keys().next();
+                        temp.put(key, jsonObject.getString(key));
+                    }
+                    sectionObject.setTimeMap(temp);
+                }
+            }
+            sectionlist.add(sectionObject);
+        }
+        courseObject.setSections(sectionlist);
+    }
+}
--- app/src/main/java/will/sscmaster/MainActivity.java
+++ app/src/main/java/will/sscmaster/MainActivity.java
@@ -2,11 +2,13 @@ package will.sscmaster;
 
 import android.content.Context;
 import android.graphics.Point;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.util.Log;
 import android.view.Display;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -19,16 +21,22 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
+import will.sscmaster.Backend.CourseObject;
+import will.sscmaster.Backend.ParseObjectData;
 import will.sscmaster.SupportingUI.MainListAdapter;
 
 public class MainActivity extends AppCompatActivity {
     public static final String FACULTY_LIST = "faculty_list";
+    public static final String ALL_DATA = "allData.json";
 
     private String[] facultyData;
     private RecyclerView mainList;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
+
+    private Set<CourseObject> courseDataset;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -72,28 +80,20 @@ public class MainActivity extends AppCompatActivity {
 
     // return an array of string in a form of "faculty_name,color"
     private String[] readFacultyList() {
-        try {
-            StringBuilder stringBuilder = new StringBuilder();
-            InputStreamReader inputStreamReader = new InputStreamReader(getAssets().open(FACULTY_LIST), "UTF-8");
-            BufferedReader br = new BufferedReader(inputStreamReader);
-            String line = br.readLine();
-            while (line != null) {
-                stringBuilder.append(line);
-                line = br.readLine();
-            }
-            br.close();
-            inputStreamReader.close();
-            String temp = stringBuilder.toString();
-            return temp.split(";");
-        } catch(IOException e) {
-            return new String[1];
-        }
+        String temp = readLocalData(FACULTY_LIST);
+        return temp.split(";");
     }
 
     private void readAllData() {
+        String temp = readLocalData(ALL_DATA);
+        courseDataset = ParseObjectData.parseJson(temp);
+    }
+
+    @NonNull
+    private String readLocalData(String require) {
+        StringBuilder stringBuilder = new StringBuilder();
         try {
-            StringBuilder stringBuilder = new StringBuilder();
-            InputStreamReader inputStreamReader = new InputStreamReader(getAssets().open("allData.json"), "UTF-8");
+            InputStreamReader inputStreamReader = new InputStreamReader(getAssets().open(require), "UTF-8");
             BufferedReader br = new BufferedReader(inputStreamReader);
             String line = br.readLine();
             while (line != null) {
@@ -102,9 +102,9 @@ public class MainActivity extends AppCompatActivity {
             }
             br.close();
             inputStreamReader.close();
-            String temp = stringBuilder.toString();
-        } catch(IOException e) {
-
+        } catch (IOException e) {
+            Log.e("ERROR", "Can't read local data");
         }
+        return stringBuilder.toString();
     }
 }
